3f4a705e08f1f9e1f434bddc5fcabf3c453dbf61
lordvoltemort <rahuldas.das2012@gmail.com>
2017-06-01 00:10:15 +0530

    .

--- bootstrap-datetimepicker-master/sample in bootstrap v3/DBImage.php
+++ bootstrap-datetimepicker-master/sample in bootstrap v3/DBImage.php
@@ -3,15 +3,13 @@
 	ini_set('default_sockey_timeout',300);
 ?>
 
-
 <html>
 <body>
-<script type="text/javascript">
+<script >
 
-function myFunction() {
-console.log('event',event.target.getAttribute("data-photoId"));
-//	var val = "<?php echo $image_id ?>";
-
+	function myFunction() {
+	//console.log('event',event.target.getAttribute("image_id"));
+	
 }
 </script>
 
@@ -21,7 +19,15 @@ console.log('event',event.target.getAttribute("data-photoId"));
 	<input type="submit" name="submit" value="upload">
 </form>
 
+</body>
+</html>
+
 <?php
+
+	$GLOBALS['items'] = array();
+	$GLOBALS['getId'] = array();
+	$GLOBALS['i'] = 0;
+
 	if (isset($_POST['submit'])) {
 		if (getimagesize($_FILES['image']['tmp_name'])== FALSE) {
 			echo "Please select an image";
@@ -35,7 +41,7 @@ console.log('event',event.target.getAttribute("data-photoId"));
 
 		}
 	}
-	displayimage();
+	
 	function saveimage($name,$image)
 	{	
 
@@ -54,27 +60,44 @@ console.log('event',event.target.getAttribute("data-photoId"));
 
 	}
 
-	
+	displayimage();
 
-	function displayimage(){
+		function displayimage(){
 		$con=mysqli_connect("localhost","Rahul","Koqa313*@3");
 		mysqli_select_db($con,"testing");
-		$qry = "select * from images";
+		$qry = "select * from images ";
 		$result = mysqli_query($con,$qry);
-
+ 		
+		
+		
 		while ($row = mysqli_fetch_array($result)) {
-			$GLOBALS['image_id'] = $row['image_id'];
 			echo '<img height ="300" width ="300" src="data:image/jpg;base64,' .$row[2]. ' "  >';
-			echo ' <input type ="submit" value = "Click to book" onclick = " myFunction()"	> ';
-			echo '  ';
-
-
+			//$GLOBALS['image_id'] = $row['image_id'];
+			$items[$i] = $row['image_id'];
+			echo "<form method = 'POST' action = '' >";
+			echo ' <input type ="submit" name = "getId[$i]" value = "Click to book"> ';
+			echo "</form>";
+			$i++;
+			//displayImageId($image_id);
 		}
+	
+	if (isset($_POST['getId[$i]'])) {
+		
+		displayImageId($items[$i]);
+		
+	}
+
+	function displayImageId($array)
+	{
+		echo $items[$i];
+
+	}
+
 		mysqli_close($con);
 	}
+	
+
+
 ?>
 
-
-</body>
-</html>
 
